b0f8a41ce41da5139d12569eda58e9659bc2008e
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-07-01 14:16:11 +0900

    feat: create index page

--- src/main/kotlin/com/example/html_demo_spring/HomeController.kt
+++ src/main/kotlin/com/example/html_demo_spring/HomeController.kt
@@ -1,4 +1,19 @@
 package com.example.html_demo_spring
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+
+@Controller
 class HomeController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
+
+    @PostMapping("/furtherInfo")
+    fun furtherInfo(): String {
+        return "furtherInfo"
+    }
 }
---
+++ src/test/kotlin/com/example/html_demo_spring/HtmlDemoSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.html_demo_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HtmlDemoSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
