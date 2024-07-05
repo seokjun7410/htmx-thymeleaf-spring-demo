e9545c2aefa8028d5aadd589aebe99759138834c
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-07-05 15:56:46 +0900

    feat: 나의 영상 목록 ui 추가

--- src/main/kotlin/com/example/html_demo_spring/HomeController.kt
+++ src/main/kotlin/com/example/html_demo_spring/HomeController.kt
@@ -1,6 +1,7 @@
 package com.example.html_demo_spring
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 
@@ -8,9 +9,24 @@ import org.springframework.web.bind.annotation.PostMapping
 class HomeController {
 
     @GetMapping("/")
-    fun index(): String {
+    fun index(model: Model): String {
+        val video1 = Video(
+            "title1",
+            "https://miro.medium.com/v2/resize:fit:1036/format:webp/0*qrHSfE71UUbFrmH6"
+        )
+        val video2 = Video(
+            "title2",
+            "https://miro.medium.com/v2/resize:fit:1036/format:webp/0*qrHSfE71UUbFrmH6"
+        )
+        val video3 = Video(
+            "title3",
+            "https://miro.medium.com/v2/resize:fit:1036/format:webp/0*qrHSfE71UUbFrmH6"
+        )
+        val videos = listOf(video1, video2, video3)
+        model.addAttribute("videos", videos)
         return "sdgb/index"
     }
+
     @PostMapping("/views/overview")
     fun overview(): String {
         return "sdgb/views/overview"
@@ -20,12 +36,35 @@ class HomeController {
     fun subscribe(): String {
         return "sdgb/views/subscribe"
     }
+
     @PostMapping("/views/rank")
     fun rank(): String {
         return "sdgb/views/rank"
     }
+
     @PostMapping("/views/score")
     fun score(): String {
         return "sdgb/views/score"
     }
+
+    @GetMapping("/views/demo")
+    fun demo(model: Model): String {
+        val video1 = Video(
+            "title1",
+            "https://miro.medium.com/v2/resize:fit:1036/format:webp/0*qrHSfE71UUbFrmH6"
+        )
+        val video2 = Video(
+            "title2",
+            "https://miro.medium.com/v2/resize:fit:1036/format:webp/0*qrHSfE71UUbFrmH6"
+        )
+        val video3 = Video(
+            "title3",
+            "https://miro.medium.com/v2/resize:fit:1036/format:webp/0*qrHSfE71UUbFrmH6"
+        )
+        val videos = listOf(video1, video2, video3)
+        model.addAttribute("videos", videos)
+        return "sdgb/views/demo"
+    }
 }
+
+data class Video(val title: String, val thumbnailUrl: String)
